473f2d09152704a93f0ddf7012af2dbb78b971a7
Chaitanya Pathak <chaitanyapathak2004@gmail.com>
2024-09-04 13:52:42 +0530

    Added New and Create Route

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get("/listings", async (req, res) =>
 {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-})
+});
 
 // Show Route
 app.get("/listings/:id", async (req, res) =>
@@ -49,7 +49,21 @@ app.get("/listings/:id", async (req, res) =>
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
+});
+
+// New Route
+app.get("/listing/new", (req, res) =>
+{
+    res.render("listings/new.ejs");
 })
+
+// Create Route
+app.post("/listings", async (req, res) =>
+{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
 
 // app.get("/testListing", async (req, res) =>
 // {
