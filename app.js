fea97c2241fb297a4845b9b75a9c412d088fd68a
Chaitanya Pathak <chaitanyapathak2004@gmail.com>
2024-09-05 19:11:42 +0530

    Started Front-End

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const Listing = require("./models/listing.js")
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require('path');
 const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 async function main()
 {
@@ -36,7 +39,6 @@ app.get("/", (req, res) =>
 {
     res.send("Hi I am root!");
 });
-
 
 // Index Route
 app.get("/listings", async (req, res) =>
